62702c131470b6677022a8a79e81a4982745ebce
Eric Oliver <olivere@futuredesigngroup.com>
2023-11-08 12:17:01 -0500

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# js-tip-calculator
-Simple JS application to use for bill and tip calculations.
+# JS Tip Calculator
+
+In this video, I will show you how to create a simple restaurant bill and tip calculator using vanilla JavaScript, HTML, and SASS. You can use this tutorial to build your own and help you figure out the total bill amount per person and even what each person will need to tip based on selectable percentages.
+
+This is a quick and easy project that anyone learning JavaScript can do. It uses Bootstrap for styling and we create a custom vanilla JavaScript class to handle the integration and calculations. All of it is wrapped up nicely in a visually appealing package that you can convert to an app or just use on your website.
+
+View the video: [https://youtu.be/GvWY0eR4zJo](https://youtu.be/GvWY0eR4zJo)
---
+++ calculator.js
@@ -0,0 +1,66 @@
+class TipCalculator {
+	constructor() {
+		// Create elements for the calculator
+		this.button = document.querySelector(".calculateBtn");
+		this.totalElement = document.querySelector(".total");
+		this.peopleElement = document.querySelector(".people");
+		this.percentageElements = document.querySelectorAll(".percentage");
+		this.resultElement = document.querySelector(".result");
+
+		// Attach the event listener in the constructor
+		this.button.addEventListener("click", () => {
+			this.calculateTip();
+		});
+	}
+
+	calculateTip() {
+		// Get the values from the inputs, format as needed
+		const total = parseFloat(this.totalElement.value);
+		const people = parseInt(this.peopleElement.value);
+		let percentage = 0;
+
+		// Use a variable to track validation errors
+		let invalid = 0;
+
+		// Remove 'is-invalid' class before revalidation
+		this.totalElement.classList.remove("is-invalid");
+		this.peopleElement.classList.remove("is-invalid");
+
+		// Validate total and people inputs
+		if (isNaN(total) || total <= 0) {
+			this.totalElement.classList.add("is-invalid");
+			invalid++;
+		}
+
+		if (isNaN(people) || people <= 0) {
+			this.peopleElement.classList.add("is-invalid");
+			invalid++;
+		}
+
+		if (invalid > 0) {
+			return;
+		}
+
+		// Find the checked percentage
+		this.percentageElements.forEach((element) => {
+			if (element.checked) {
+				percentage = parseInt(element.value);
+			}
+		});
+
+		// Calculate tip and total per person
+		const tipAmount = (total * (percentage / 100)) / people;
+		const totalPerPerson = total / people + tipAmount;
+
+		// Update the result element
+		this.resultElement.querySelector(
+			".tipamount"
+		).textContent = `$${tipAmount.toFixed(2)}`;
+		this.resultElement.querySelector(
+			".perperson"
+		).textContent = `$${totalPerPerson.toFixed(2)}`;
+	}
+}
+
+// Create an instance of the TipCalculator
+const calculator = new TipCalculator();
